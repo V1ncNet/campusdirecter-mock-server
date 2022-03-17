87df3d8cdeb5755fbe092607dfd5c7f5e490a7b5
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-03-17 14:32:06 +0100

    Obtain student ID from JWT

--- src/app/student/presentation/student.controller.ts
+++ src/app/student/presentation/student.controller.ts
@@ -1,20 +1,20 @@
 import { Request, Response } from 'express';
 import { controller, get } from '../../../lib/web/bind/annotations';
-import { NotFoundError } from '../../../lib/http';
+import { InternalServerError, NotFoundError } from '../../../lib/http';
 import { Controller } from '../../../lib/web';
 import { studentRepository } from '../../../index';
 import { Student, StudentId } from '../model/student';
+import { authorized } from '../../security/app';
 
 @controller('/student')
 export default class StudentController extends Controller {
-  @get('/')
-  getDefault(req: Request, res: Response) {
-    return res.redirect(req.url + '/0815421337420');
-  }
-
-  @get('/:id')
+  @get('/', authorized)
   get(req: Request, res: Response) {
-    const id: StudentId = String(req.params.id);
+    const id: StudentId = String(res.locals.user.username);
+    if (typeof res.locals.user?.username !== 'string' || !id) {
+      throw new InternalServerError('JWT payload expected to contain data but none were provided', req);
+    }
+
     const student: Student = studentRepository.retrieve(id);
 
     if (!student) {
